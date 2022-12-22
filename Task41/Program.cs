9b0f96e7096d9bd4bfed7651d40892822e0d4691
Kailar0000 <kailar0000@gmail.com>
2022-12-22 18:52:33 +0300

    Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Вводите числа через пробел:");
+int[] s = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int sum=0;
+foreach (int element in s)
+{
+	if(element>0)
+		sum++;
+}
+Console.WriteLine($"Количество положительных чисел равно {sum}");
