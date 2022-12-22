a43f28c3dd87b8e0efaf9912fee653104d079ec8
Kailar0000 <kailar0000@gmail.com>
2022-12-22 19:52:28 +0300

    Task7b

---
+++ Task7b/Program.cs
@@ -0,0 +1,19 @@
+﻿double Length(int[] x ,int[] y )
+{
+    double xy =0;
+    xy = Math.Round(Math.Sqrt(Math.Pow( y[1]-x[1], 2 )+Math.Pow( y[2]-x[2], 2 )));
+    return xy;
+}
+Console.Clear();
+Console.WriteLine("Вводите числа через пробел:");
+int[] a = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.WriteLine("Вводите числа через пробел:");
+int[] b = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.WriteLine("Вводите числа через пробел:");
+int[] c = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+double ab=Length(a, b);
+double bc=Length(b, c);
+double ac=Length(a, c);
+double p=(ab+bc+ac)/2;
+double S=Math.Round(Math.Sqrt(p(p-a)(p-b)(p-c)));
+Console.WriteLine(S);
